598077eb7b5491e8d59705b8ccf9bf4d10db7987
Guanglin <hi@guanglin.me>
2022-02-03 00:52:11 +0800

    完善类的私有属性说明

--- src/OOP/class.py
+++ src/OOP/class.py
@@ -21,9 +21,11 @@ def run(self):
 
 
 dog1 = Dog('xiaohei', 'black')
-# 无法从外部直接访问私有变量
+# 私有属性不能被公开访问，也不能被子类访问
 # print(dog1.__color)
-# print(dog1._Dog__color)
+assert not hasattr(dog1, '__color')
+# 可从__dict__中查看私有属性的真实名称
+print(dog1._Dog__color)
 dog1.get_color()
 dog1.set_color('green')
 dog1.get_color()
@@ -31,12 +33,12 @@ dog1.get_color()
 # 不能从外部直接修改私有变量
 # dog1._Dog__color = 'yellow'
 dog1.__color = 'yellow'
-print(dog1.__color)
+print('New attribute: ', dog1.__color)
 dog1.get_color()
-
 del dog1.name
 print(dog1.name)  # 实例属性可将类属性屏蔽
 print(Dog.name)
+# print(Dog._age)
 
 # 可动态添加类和实例的属性、方法
 Dog.name = 'Droppy'
