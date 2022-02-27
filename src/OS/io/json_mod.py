5896d0dddc58c45477dcc12beae0f4fd92ef8193
Guanglin <hi@guanglin.me>
2022-02-27 22:19:11 +0800

    完善json操作

--- src/OS/io/json_mod.py
+++ src/OS/io/json_mod.py
@@ -6,7 +6,7 @@ import os
 
 os.chdir('res')
 
-data_dict = {'name': '张三', 'age': 25, 'is_marriage': False, 'address': {'city': 'Shanghai', 'street': 'Nanjing Road'},
+data_dict = {'name': '张三', 'age': 25, 'married': False, 'address': {'city': 'Shanghai', 'street': 'Nanjing Road'},
              'hobbies': ['吹', '拉', '弹', '唱']}
 
 json_dump = json.dumps(data_dict)
@@ -18,10 +18,13 @@ print(f'\nPretty json=>\n{json_pretty}')
 data_load = json.loads(json_dump)
 print(f"\nGet value: {data_load['address']['street']}")
 
-
-with open('test.json', 'w') as f:
+with open('test.json', 'w', encoding='utf-8') as f:
     json.dump(data_dict, f, ensure_ascii=False, indent=4, sort_keys=True)
 
 print('\nLoad json data from a file=>')
-with open('test.json') as f:
+with open('test.json', encoding='utf-8') as f:
     print(json.load(f))
+
+
+# 命令行操作
+# python -m json.tool test.json
